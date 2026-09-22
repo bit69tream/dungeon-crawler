f67d898e05b34ac981c113dacb47da565808c594
Yaroslav Kargapolov <bit69tream@tuta.io>
2026-09-23 00:15:25 +0300

    Some fixes and style changes

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from enum import Enum
 from random import choice, randint
 
 
-class TileType(Enum):
-    EMPTY = " "
-    PLAYER = "@"
-    GROUND = "."
-    WALL = "#"
-    CHEST = "m"
-    DOOR = "|-"
-    RAT = "r"
-    ZOMBIE = "z"
-    KOBOLD = "k"
-
-
 class Direction(Enum):
     RIGHT = (1, 0)
     LEFT = (-1, 0)
@@ -65,13 +53,25 @@ player: Player
 MAP_WIDTH = 80
 MAP_HEIGHT = 24
 DRUNK_WALK_DEPTH_LIMIT = 50
-MAP_FILL_PERCENT = 0.3
+MAP_FILL_RATIO = 0.3
 
-map = [[TileType.EMPTY.value for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
+TILE_GLYPHS = {
+    "empty": " ",
+    "player": "@",
+    "ground": ".",
+    "wall": "#",
+    "chest": "m",
+    "rat": "r",
+    "zombie": "z",
+    "kobold": "k",
+}
 
 
-def how_much_map_is_generated():
-    amount_of_ground = sum([l.count(TileType.GROUND.value) for l in map])
+map = [[TILE_GLYPHS["empty"] for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
+
+
+def map_fill_ratio():
+    amount_of_ground = sum([l.count(TILE_GLYPHS["ground"]) for l in map])
     if amount_of_ground == 0:
         return 0
     return amount_of_ground / (MAP_WIDTH * MAP_HEIGHT)
@@ -90,7 +90,7 @@ def random_position_on_ground() -> Position:
         (x, y)
         for y, l in enumerate(map)
         for x, c in enumerate(l)
-        if c == TileType.GROUND.value
+        if c == TILE_GLYPHS["ground"]
     ]
     while True:
         try:
@@ -115,7 +115,7 @@ def drunk_walk(start: Position):
         if not is_position_valid(current_pos):
             break
 
-        map[current_pos.y][current_pos.x] = TileType.GROUND.value
+        map[current_pos.y][current_pos.x] = TILE_GLYPHS["ground"]
         depth += 1
 
 
@@ -123,22 +123,28 @@ def place_walls():
     directions = [x.value for x in Direction]
     for y in range(MAP_HEIGHT):
         for x in range(MAP_WIDTH):
-            if map[y][x] != TileType.EMPTY.value:
+            if map[y][x] != TILE_GLYPHS["empty"]:
                 continue
             for d in directions:
                 xd = x + d[0]
                 yd = y + d[1]
                 if (
                     is_position_valid(Position(xd, yd))
-                    and map[yd][xd] == TileType.GROUND.value
+                    and map[yd][xd] == TILE_GLYPHS["ground"]
                 ):
-                    map[y][x] = TileType.WALL.value
+                    map[y][x] = TILE_GLYPHS["wall"]
 
     for x in range(MAP_WIDTH):
-        if map[0][x] == TileType.GROUND.value:
-            map[0][x] = TileType.WALL.value
-        if map[-1][x] == TileType.GROUND.value:
-            map[-1][x] = TileType.WALL.value
+        if map[0][x] == TILE_GLYPHS["ground"]:
+            map[0][x] = TILE_GLYPHS["wall"]
+        if map[-1][x] == TILE_GLYPHS["ground"]:
+            map[-1][x] = TILE_GLYPHS["wall"]
+
+    for y in range(MAP_HEIGHT):
+        if map[y][0] == TILE_GLYPHS["ground"]:
+            map[y][0] = TILE_GLYPHS["wall"]
+        if map[y][-1] == TILE_GLYPHS["ground"]:
+            map[y][-1] = TILE_GLYPHS["wall"]
 
 
 def place_entities():
@@ -147,13 +153,13 @@ def place_entities():
 
 def generate_map():
     generated_percent = 0
-    while generated_percent < MAP_FILL_PERCENT:
+    while generated_percent < MAP_FILL_RATIO:
         if generated_percent == 0:
             start_pos = Position(MAP_WIDTH // 2, MAP_HEIGHT // 2)
         else:
             start_pos = random_position_on_ground()
         drunk_walk(start_pos)
-        generated_percent = how_much_map_is_generated()
+        generated_percent = map_fill_ratio()
     place_walls()
     place_entities()
 
