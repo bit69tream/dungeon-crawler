18435bd6db0ecaeab045caa7a5f5a245c5065281
Yaroslav Kargapolov <bit69tream@tuta.io>
2026-09-23 12:46:11 +0300

    Colored map output

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from copy import deepcopy
 from dataclasses import dataclass
 from random import choice, randint
 from typing import Self
+
+from termcolor import cprint
 
 
 @dataclass
@@ -29,19 +32,27 @@ class Position(Vec2):
     pass
 
 
+@dataclass
+class MapGlyph:
+    glyph: str
+    text_color: str
+    text_attributes: list[str]
+
+
 MAP_WIDTH = 80
 MAP_HEIGHT = 24
 DRUNK_WALK_DEPTH_LIMIT = 50
 MAP_FILL_RATIO = 0.3
 TILE_GLYPHS = {
-    "empty": " ",
-    "player": "@",
-    "ground": ".",
-    "wall": "#",
-    "chest": "m",
-    "rat": "r",
-    "zombie": "z",
-    "kobold": "k",
+    "empty": MapGlyph(" ", "white", []),
+    "unknown": MapGlyph("?", "white", []),
+    "player": MapGlyph("@", "white", ["reverse"]),
+    "ground": MapGlyph(".", "dark_grey", []),
+    "wall": MapGlyph("#", "light_grey", []),
+    "chest": MapGlyph("m", "yellow", ["bold"]),
+    "rat": MapGlyph("r", "red", []),
+    "zombie": MapGlyph("z", "green", []),
+    "kobold": MapGlyph("k", "blue", []),
 }
 DIRECTIONS = {
     "right": Vec2(1, 0),
@@ -59,6 +70,7 @@ class Entity:
     def __init__(self, position: Position, health: int) -> None:
         self.position: Position = position
         self.health: int = health
+        self.type: str = "unknown"
 
 
 class Enemy(Entity):
@@ -69,26 +81,31 @@ class Enemy(Entity):
 class Player(Entity):
     def __init__(self, position: Position, health: int) -> None:
         super().__init__(position, health)
+        self.type: str = "player"
 
 
 class Chest(Entity):
     def __init__(self, position: Position, health: int) -> None:
         super().__init__(position, health)
+        self.type: str = "chest"
 
 
 class Rat(Enemy):
     def __init__(self, position: Position, health: int) -> None:
         super().__init__(position, health)
+        self.type: str = "rat"
 
 
 class Zombie(Enemy):
     def __init__(self, position: Position, health: int) -> None:
         super().__init__(position, health)
+        self.type: str = "zombie"
 
 
 class Kobold(Enemy):
     def __init__(self, position: Position, health: int) -> None:
         super().__init__(position, health)
+        self.type: str = "kobold"
 
 
 NON_PLAYABLE_ENTITIES = {
@@ -102,7 +119,8 @@ NON_PLAYABLE_ENTITIES = {
 class Dungeon:
     def __init__(self, map_width: int, map_height: int):
         self.map: list[list[str]] = [
-            [TILE_GLYPHS["empty"] for _ in range(map_width)] for _ in range(map_height)
+            [TILE_GLYPHS["empty"].glyph for _ in range(map_width)]
+            for _ in range(map_height)
         ]
         self._entities: list[Entity] = []
         self._player: Player
@@ -112,7 +130,7 @@ class Dungeon:
 
         self.generate_map()
 
-    def _map_fill_ratio(self, glyph: str = TILE_GLYPHS["ground"]):
+    def _map_fill_ratio(self, glyph: str = TILE_GLYPHS["ground"].glyph):
         amount_of_ground = sum([row.count(glyph) for row in self.map])
         if amount_of_ground == 0:
             return 0
@@ -129,7 +147,7 @@ class Dungeon:
             Vec2(x, y)
             for y, l in enumerate(self.map)
             for x, c in enumerate(l)
-            if c == TILE_GLYPHS["ground"]
+            if c == TILE_GLYPHS["ground"].glyph
         ]
         while True:
             try:
@@ -160,7 +178,7 @@ class Dungeon:
             if not picked_a_future:
                 return
 
-            self.map[current_pos.y][current_pos.x] = TILE_GLYPHS["ground"]
+            self.map[current_pos.y][current_pos.x] = TILE_GLYPHS["ground"].glyph
             depth += 1
 
         self._possible_entity_locations.append(current_pos)
@@ -168,33 +186,33 @@ class Dungeon:
     def _place_walls(self):
         for y in range(self.map_height):
             for x in range(self.map_width):
-                if self.map[y][x] != TILE_GLYPHS["empty"]:
+                if self.map[y][x] != TILE_GLYPHS["empty"].glyph:
                     continue
                 for d in DIRECTIONS.values():
                     pos = Position(x + d.x, y + d.y)
                     if (
                         self._is_map_position_valid(pos)
-                        and self.map[pos.y][pos.x] == TILE_GLYPHS["ground"]
+                        and self.map[pos.y][pos.x] == TILE_GLYPHS["ground"].glyph
                     ):
-                        self.map[y][x] = TILE_GLYPHS["wall"]
+                        self.map[y][x] = TILE_GLYPHS["wall"].glyph
 
         for x in range(self.map_width):
-            if self.map[0][x] == TILE_GLYPHS["ground"]:
-                self.map[0][x] = TILE_GLYPHS["wall"]
-            if self.map[-1][x] == TILE_GLYPHS["ground"]:
-                self.map[-1][x] = TILE_GLYPHS["wall"]
+            if self.map[0][x] == TILE_GLYPHS["ground"].glyph:
+                self.map[0][x] = TILE_GLYPHS["wall"].glyph
+            if self.map[-1][x] == TILE_GLYPHS["ground"].glyph:
+                self.map[-1][x] = TILE_GLYPHS["wall"].glyph
 
         for y in range(self.map_height):
-            if self.map[y][0] == TILE_GLYPHS["ground"]:
-                self.map[y][0] = TILE_GLYPHS["wall"]
-            if self.map[y][-1] == TILE_GLYPHS["ground"]:
-                self.map[y][-1] = TILE_GLYPHS["wall"]
+            if self.map[y][0] == TILE_GLYPHS["ground"].glyph:
+                self.map[y][0] = TILE_GLYPHS["wall"].glyph
+            if self.map[y][-1] == TILE_GLYPHS["ground"].glyph:
+                self.map[y][-1] = TILE_GLYPHS["wall"].glyph
 
     def _place_entities(self):
         possible_entity_locations = [
             p
             for p in self._possible_entity_locations
-            if self.map[p.y][p.x] == TILE_GLYPHS["ground"]
+            if self.map[p.y][p.x] == TILE_GLYPHS["ground"].glyph
         ]
         if len(possible_entity_locations) == 0:
             raise RuntimeError("BUG!")
@@ -224,17 +242,29 @@ class Dungeon:
         self._place_walls()
         self._place_entities()
 
-    def print_map(self):
+    def print_map(self, display_entites: bool = True):
+        current_map = deepcopy(self.map)
+
+        if display_entites:
+            current_map[self._player.position.y][self._player.position.x] = TILE_GLYPHS[
+                self._player.type
+            ].glyph
+
+            for e in self._entities:
+                current_map[e.position.y][e.position.x] = TILE_GLYPHS[e.type].glyph
+
         for y in range(self.map_height):
             for x in range(self.map_width):
-                print(self.map[y][x], end="")
+                tile = next(
+                    t for t in TILE_GLYPHS.values() if t.glyph == current_map[y][x]
+                )
+                cprint(tile.glyph, tile.text_color, attrs=tile.text_attributes, end="")
             print()
 
 
 def main():
     dungeon = Dungeon(MAP_WIDTH, MAP_HEIGHT)
     dungeon.print_map()
-    print(dungeon._entities)
 
 
 if __name__ == "__main__":
